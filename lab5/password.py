d2f3e0a00fb07d5b02357a1e81b5445b1e9331af
p83218882 <qrypto19@gmail.com>
2020-11-16 10:56:08 +0000

    .

--- lab5/password.py
+++ lab5/password.py
@@ -1,11 +1,18 @@
 psw = str(input('password: '))
 password = 'abc123'
-while  psw != password :
-  print('wrong password')
-  break
+i = 0
+while i < float("inf") :
+    i+=1   
 
-if psw == 'help' :
-  print(password[0])
+    if psw == password :
+        print('welcome')
+        break
 
-elif psw == True :
-  print('welcome')
+    elif psw == 'help' :
+        print(password[0])
+        break
+       
+    elif psw!= password and psw != 'help' :
+    
+            print('wrong, try again ')
+            psw = str(input('password: '))
